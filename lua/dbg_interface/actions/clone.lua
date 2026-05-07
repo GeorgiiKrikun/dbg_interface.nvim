7b3bf75f83e5062e5486c8600b65d0238aa53bbb
Georgii <georgii.krikun@celantur.com>
2026-05-07 13:00:20 +0200

    Add targets

---
+++ lua/dbg_interface/actions/clone.lua
@@ -0,0 +1,53 @@
+local async = require('plenary.async')
+local select = require('dbg_interface.select')
+local edit_ui = require('dbg_interface.edit_ui')
+local DbgTarget = require('dbg_interface.DbgTarget')
+local DbgArguments = require('dbg_interface.DbgArguments')
+
+local done = function(callback, payload)
+    if callback then callback(payload) end
+end
+
+local M = {}
+
+M.target = function(config, callback)
+    async.run(
+        function()
+            local copied_config = vim.deepcopy(config)
+            local selected_type = select.type_async(copied_config)
+            if not selected_type then
+                done(callback, nil)
+                return
+            end
+
+            local template = select.target_async(selected_type)
+            if not template then
+                done(callback, nil)
+                return
+            end
+
+            local template_copy = vim.deepcopy(template)
+            local new_target = edit_ui.edit_stuff_async(template_copy, DbgTarget)
+
+            if selected_type:path_exists(new_target.relpath) then
+                vim.notify("New executable path already exists", vim.log.levels.ERROR)
+                done(callback, nil)
+                return
+            end
+
+            table.insert(selected_type.targets, new_target)
+            done(callback, copied_config)
+        end,
+        function(err)
+            if err then
+                vim.notify("An error occurred: " .. tostring(err), vim.log.levels.ERROR)
+            end
+        end
+    )
+end
+
+M.args = function(config, callback)
+    -- TODO: implement
+end
+
+return M
