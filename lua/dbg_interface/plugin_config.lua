a2100c00a67dd9698d9cf916cca04f3eead69a4e
Georgii <georgii.krikun@celantur.com>
2026-05-07 17:15:49 +0200

    Progress

--- lua/dbg_interface/plugin_config.lua
+++ lua/dbg_interface/plugin_config.lua
@@ -1,9 +1,11 @@
+local keymaps = require('dbg_interface.keymaps')
 local M = {}
 
 local configs = {}
 
 function M.setup(user_opts)
     configs = vim.tbl_deep_extend('force', configs, user_opts or {})
+    keymaps.setup(configs.keymaps)
 end
 
 function M.get()
