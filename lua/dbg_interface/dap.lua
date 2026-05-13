4eddd5321c9cd819bb4044691e15cba59561ce1a
Georgii <georgii.krikun@celantur.com>
2026-05-13 16:50:42 +0200

    Some progress

---
+++ lua/dbg_interface/dap.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+local plugin_config = require('dbg_interface.plugin_config')
+
+--- Build a ready-to-use nvim-dap launch config from the plugin's data model.
+--- @param dbg_type DbgType  the type entry (carries debug_type string)
+--- @param target   DbgTarget  the target entry (relpath, alias, executable_type)
+--- @param args     DbgArguments|nil  the argument preset, or nil for no args
+--- @return table  a DAP configuration table suitable for dap.run()
+function M.to_dap_config(dbg_type, target, args)
+    local type_entry_config = vim.tbl_get(plugin_config.get(), 'debug_types', dbg_type.debug_type, 'config')
+
+    local name = target.alias
+    if args and args.alias and args.alias ~= "no args" then
+        name = name .. " [" .. args.alias .. "]"
+    end
+
+    return vim.tbl_deep_extend('force', type_entry_config, {
+        name = name,
+        program = vim.fn.fnamemodify(target.relpath, ":p"),
+        args = (args and args.args) or {},
+        cwd = vim.fn.getcwd(),
+    })
+end
+
+return M
--- lua/dbg_interface/init.lua
+++ lua/dbg_interface/init.lua
@@ -3,6 +3,7 @@ local M = {}
 local plugin_config = require('dbg_interface.plugin_config')
 local dbg_config = require('dbg_interface.config')
 local actions = require('dbg_interface.actions')
+local dap = require('dbg_interface.dap')
 
 M.local_dbg_config = dbg_config.local_dbg_config
 M.save_debug_config = dbg_config.save
@@ -11,6 +12,8 @@ M.add = actions.add
 M.edit = actions.edit
 M.remove = actions.remove
 M.clone = actions.clone
+
+M.to_dap_config = dap.to_dap_config
 
 M.setup = plugin_config.setup
 
